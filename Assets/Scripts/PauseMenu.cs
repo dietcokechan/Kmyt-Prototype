c50adbf5855db8dad521a7ddab9e690a8d74f556
dietcokechan <dietcokechan@gmail.com>
2022-05-16 01:03:34 +0200

    pause menu changes

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        gameObject.SetActive(true);
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        gameObject.SetActive(false);
+    }
+}
