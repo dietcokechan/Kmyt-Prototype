a406e6233e4eeb19aec4f8ecf6062b8bfd73caeb
dietcokechan <dietcokechan@gmail.com>
2022-05-16 01:03:07 +0200

    comments!

--- Assets/Scripts/CrocodilesAnimController.cs
+++ Assets/Scripts/CrocodilesAnimController.cs
@@ -15,18 +15,22 @@ public class CrocodilesAnimController : MonoBehaviour
 
     void Start()
     {
+        // Grab components
         anim = GetComponentInChildren<Animator>();
         oathHandler = tree.GetComponentInChildren<OathHandler>();
     }
 
     void Update()
     {
+        // by default play the idle animation
         Idle();
 
+        // if the player is near and enemy can attack then play attack animation
         if(canAttack && playerNear)
         {
             Attack();
         }
+        // if the player is near but has read the oath then play the back to idle animation
         if(!canAttack && playerNear)
         {
             BackToIdle();
@@ -52,11 +56,13 @@ public class CrocodilesAnimController : MonoBehaviour
     {
         playerNear = true;
 
+        // if the oath is not read then enemy can attack
         if(!oathHandler.oathRead)
         {
             canAttack = true;
         }
 
+        // if the oath is read then enemy cannot attack
         if(oathHandler.oathRead)
         {
             canAttack = false;
